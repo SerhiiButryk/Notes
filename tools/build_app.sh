afdc8dce1d1fd71a96368aa9cf302bca6b151843
Serhii Butryk <sergeybutr@gmail.com>
2022-05-08 19:06:48 +0300

    333

--- tools/build_app.sh
+++ tools/build_app.sh
@@ -3,14 +3,39 @@
 # Copyright 2022. Happy coding ! :)
 # Author: Serhii Butryk 
 
+# Simple script for building Notes App.
+
+# Fail if somthing is wrong
+set -e
+
+# Add untility scripts
+SCRIPT_RELEVANT_PATH=$( dirname $BASH_SOURCE[0] )
+. ${SCRIPT_RELEVANT_PATH}/utility_functions.sh
+
 SCRIPT_ABSOLUTE_PATH="$( dirname $( pwd )$(cut -c 2- <<< $0) )"
 
-echo $SCRIPT_ABSOLUTE_PATH
+BUILD_FOLDER="${SCRIPT_ABSOLUTE_PATH}/../Notes/"
+APK_FILES_FOLDER="${SCRIPT_ABSOLUTE_PATH}/../Notes/app/build/outputs/apk"
+MAPPING_FOLDER="${SCRIPT_ABSOLUTE_PATH}/../Notes/app/build/outputs/mapping"
+ARTIFACT_FOLDER_NAME="dist"
 
-pwd
+print_message "******** Started building *********"
 
-# pushd ${SCRIPT_ABSOLUTE_PATH}/../Notes/
+# Build app
+pushd ${BUILD_FOLDER}
+./gradlew clean assemble
+popd
 
-./${SCRIPT_ABSOLUTE_PATH}/../Notes/gradlew clean assemble
+print_message "******** Finished *********"
 
-# popd
+print_message "******** Prepare artifacts *********"
+
+pushd ${SCRIPT_ABSOLUTE_PATH}/../
+# Create folder for artifacts 
+mkdir $ARTIFACT_FOLDER_NAME
+popd 
+
+cp -rf -v ${APK_FILES_FOLDER}/* ${SCRIPT_ABSOLUTE_PATH}/../${ARTIFACT_FOLDER_NAME}
+cp -rf -v ${MAPPING_FOLDER}/* ${SCRIPT_ABSOLUTE_PATH}/../${ARTIFACT_FOLDER_NAME}
+
+print_message "******** Finished *********"
