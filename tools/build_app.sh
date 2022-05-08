ea2ce7b40528b7e49a12e3b7c2b9248dfb340ee9
Serhii Butryk <sergeybutr@gmail.com>
2022-05-08 19:17:32 +0300

    Update 4

--- tools/build_app.sh
+++ tools/build_app.sh
@@ -16,26 +16,29 @@ SCRIPT_ABSOLUTE_PATH="$( dirname $( pwd )$(cut -c 2- <<< $0) )"
 
 BUILD_FOLDER="${SCRIPT_ABSOLUTE_PATH}/../Notes/"
 APK_FILES_FOLDER="${SCRIPT_ABSOLUTE_PATH}/../Notes/app/build/outputs/apk"
-MAPPING_FOLDER="${SCRIPT_ABSOLUTE_PATH}/../Notes/app/build/outputs/mapping"
+MAPPING_FOLDER="${SCRIPT_ABSOLUTE_PATH}/../Notes/app/build/outputs/mapping/release"
 ARTIFACT_FOLDER_NAME="dist"
+MAPPING_FOLDER_NAME="mapping"
 
-print_message "******** Started building *********"
+echo "******** Started building *********"
 
 # Build app
 pushd ${BUILD_FOLDER}
 ./gradlew clean assemble
 popd
 
-print_message "******** Finished *********"
+echo "******** Finished *********"
 
-print_message "******** Prepare artifacts *********"
+echo "******** Prepare artifacts *********"
 
 pushd ${SCRIPT_ABSOLUTE_PATH}/../
 # Create folder for artifacts 
-mkdir $ARTIFACT_FOLDER_NAME
+mkdir -p $ARTIFACT_FOLDER_NAME/${MAPPING_FOLDER_NAME}
 popd 
 
-cp -rf -v ${APK_FILES_FOLDER}/* ${SCRIPT_ABSOLUTE_PATH}/../${ARTIFACT_FOLDER_NAME}
-cp -rf -v ${MAPPING_FOLDER}/* ${SCRIPT_ABSOLUTE_PATH}/../${ARTIFACT_FOLDER_NAME}
+echo "Copying files"
 
-print_message "******** Finished *********"
+cp -rf -v ${APK_FILES_FOLDER}/* ${SCRIPT_ABSOLUTE_PATH}/../${ARTIFACT_FOLDER_NAME}
+cp -rf -v ${MAPPING_FOLDER}/* ${SCRIPT_ABSOLUTE_PATH}/../${ARTIFACT_FOLDER_NAME}/${MAPPING_FOLDER_NAME}
+
+echo "******** Finished *********"
